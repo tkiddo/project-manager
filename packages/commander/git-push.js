c299dc67e61c0134370d4c40bc4b0b4533d33788
tangkaiqiang <justforgit@163.com>
2020-07-10 13:38:46 +0800

    again

--- packages/commander/git-push.js
+++ packages/commander/git-push.js
@@ -1,6 +1,5 @@
 const inquirer = require('inquirer')
 const handleExec = require('../lib/handleExec')
-const lint = require('../commander/lint')
 
 module.exports = async function () {
   const answer = await inquirer.prompt({
@@ -8,7 +7,6 @@ module.exports = async function () {
     name: 'message',
     message: 'message?'
   })
-  await lint()
   const command = `git pull && git add . && git commit -m "${answer.message}" && git push origin master`
   const message = 'publishing...'
   handleExec(command)(message)
