8737098517a0312aaf6a6cb6021a3de3a0033e4f
tangkaiqiang <justforgit@163.com>
2020-07-16 16:09:30 +0800

    normal project template

---
+++ packages/templates/init/normal-project/template/src/index.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import logo from './assets/logo.png';
+
+const root = document.getElementById('root');
+
+const img = document.createElement('img');
+
+img.src = logo;
+
+root.appendChild(img);
