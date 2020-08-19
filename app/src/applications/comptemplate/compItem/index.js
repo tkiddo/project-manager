35486b38249925cfcc763504fe5542e4a4c593ba
tangkaiqiang <justforgit@163.com>
2020-08-19 14:13:41 +0800

    update

--- app/src/applications/comptemplate/compItem/index.js
+++ app/src/applications/comptemplate/compItem/index.js
@@ -1,2 +1,23 @@
 import React from 'react';
 import './index.scss';
+import { Card, Button } from 'react-bootstrap';
+
+const ComponentItem = (props) => {
+  const { name, description } = props;
+  return (
+    <Card className="comp-item">
+      <Card.Header>{name}</Card.Header>
+      <Card.Body>{description}</Card.Body>
+      <div>
+        <Button size="sm" variant="success">
+          应用
+        </Button>
+        <Button size="sm" variant="danger">
+          预览
+        </Button>
+      </div>
+    </Card>
+  );
+};
+
+export default ComponentItem;
--- app/src/applications/comptemplate/index.js
+++ app/src/applications/comptemplate/index.js
@@ -2,15 +2,26 @@ import React, { useState } from 'react';
 import './index.scss';
 import { Container, Tabs, Tab } from 'react-bootstrap';
 import CustomScroll from '../../components/CustomScroll';
+import CompItem from './compItem';
+import ComponentItem from './compItem';
 
 const CompTemplate = () => {
   const [key, setKey] = useState('react');
+  const [list, setList] = useState([
+    {
+      name: 'react-buttons',
+      description: 'button with react'
+    }
+  ]);
   return (
     <Container fluid className="padding-top-10">
       <Tabs activeKey={key} onSelect={(k) => setKey(k)}>
         <Tab eventKey="react" title="React组件">
           <CustomScroll height="600px">
-            <div className="comp-manage-list">content</div>
+            {list.map((item) => {
+              const { name, description } = item;
+              return <ComponentItem name={name} description={description} />;
+            })}
           </CustomScroll>
         </Tab>
         <Tab eventKey="vue" title="Vue组件">
